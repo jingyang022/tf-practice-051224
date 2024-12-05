1ecca55544747e7ef13e7fc070d2a8db75957770
jingyang022 <jingyang022@yahoo.com.sg>
2024-12-05 22:11:43 +0800

    updated main

--- main.tf
+++ main.tf
@@ -46,3 +46,64 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     Environment = "test"
   }
 }
+
+variable "name" {
+  description ="name of application"
+  type = string
+  default = "yap"
+}
+
+variable "vpc_id" {}
+
+data "aws_vpc" "vpc" {
+  id = var.vpc_id
+}
+
+data "aws_subnets" "public" {
+  filter{
+    name = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+  filter {
+    name = "tag:Name"
+    values = ["public-*"]
+  }
+}
+
+resource "aws_instance" "public" {
+    ami = "ami-04c913012f8977029"
+    instance_type = "t2.micro"
+    subnet_id = data.aws_subnets.public.ids[1] #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
+    associate_public_ip_address = true
+    #key_name = "yap-231124" #Change to your keyname, e.g. jazeel-key-pair
+    vpc_security_group_ids = [aws_security_group.example.id]
+
+    tags = {
+        Name = "${var.name}-dynamodb-reader"
+        }
+}
+
+resource "aws_security_group" "example" {
+    name_prefix = "${var.name}-sg" #Security group name, e.g. jazeel-terraform-security-group
+    description = "Security group for EC2"
+    vpc_id = data.aws_vpc.vpc.id #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
+    lifecycle {
+      create_before_destroy = true
+    }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+    security_group_id = aws_security_group.example.id
+    cidr_ipv4 = "0.0.0.0/0"
+    from_port = 22
+    ip_protocol = "tcp"
+    to_port = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.example.id
+    cidr_ipv4 = "0.0.0.0/0"
+    from_port = 443
+    ip_protocol = "tcp"
+    to_port = 443
+}
