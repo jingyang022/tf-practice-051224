cc8de8ff8665289dca115870096759f16ac184bd
jingyang022 <jingyang022@yahoo.com.sg>
2024-12-05 23:04:19 +0800

    updated main.tf

--- main.tf
+++ main.tf
@@ -53,16 +53,17 @@ variable "name" {
   default = "yap"
 }
 
-variable "vpc_id" {}
-
-data "aws_vpc" "vpc" {
-  id = var.vpc_id
+data "aws_vpc" "selected" {
+  filter {
+    name = "tag:Name"
+    values = ["shared-vpc"]
+  }
 }
 
 data "aws_subnets" "public" {
   filter{
     name = "vpc-id"
-    values = [data.aws_vpc.vpc.id]
+    values = [data.aws_vpc.selected.id]
   }
   filter {
     name = "tag:Name"
@@ -73,7 +74,7 @@ data "aws_subnets" "public" {
 resource "aws_instance" "public" {
     ami = "ami-04c913012f8977029"
     instance_type = "t2.micro"
-    subnet_id = data.aws_subnets.public.ids[1] #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
+    subnet_id = data.aws_subnets.public.ids[0] #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
     associate_public_ip_address = true
     #key_name = "yap-231124" #Change to your keyname, e.g. jazeel-key-pair
     vpc_security_group_ids = [aws_security_group.example.id]
@@ -86,7 +87,7 @@ resource "aws_instance" "public" {
 resource "aws_security_group" "example" {
     name_prefix = "${var.name}-sg" #Security group name, e.g. jazeel-terraform-security-group
     description = "Security group for EC2"
-    vpc_id = data.aws_vpc.vpc.id #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
+    vpc_id = data.aws_vpc.selected.id #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
     lifecycle {
       create_before_destroy = true
     }
---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "vpc_id" {
+    value = data.aws_vpc.selected.id
+}
+
+output "public_subnet_ids" {
+    value = data.aws_subnets.public.ids
+}
+
