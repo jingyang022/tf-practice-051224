c8e0fd7e6d479afb296117f6cdf33479bbe22d9f
jingyang022 <jingyang022@yahoo.com.sg>
2024-12-07 10:58:45 +0800

    corrected subnet id error

--- main.tf
+++ main.tf
@@ -4,33 +4,33 @@ variable "name" {
   default = "yap"
 }
 
-data "aws_vpc" "default" {
+/* data "aws_vpc" "default" {
   default = true
-} 
+} */ 
 
-/* data "aws_vpc" "selected" {
+data "aws_vpc" "selected" {
   filter {
     name = "tag:Name"
     values = ["shared-vpc"]
   }
-} */
+}
 
 data "aws_subnets" "public" {
   filter{
     name = "vpc-id"
-    values = [data.aws_vpc.default.id]
+    values = [data.aws_vpc.selected.id]
   }
   filter {
     name = "tag:Name"
-    values = ["public-*"]
+    values = ["*public-*"]
   }
 }
 
 resource "aws_instance" "public" {
     ami = "ami-04c913012f8977029"
     instance_type = "t2.micro"
-    subnet_id = "subnet-004425cdf7e7a28a8"
-    #subnet_id = data.aws_subnets.public.ids[1] #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
+    #subnet_id = "subnet-004425cdf7e7a28a8"
+    subnet_id = data.aws_subnets.public.ids[0] #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
     associate_public_ip_address = true
     iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
     #key_name = "yap-231124" #Change to your keyname, e.g. jazeel-key-pair
@@ -44,7 +44,7 @@ resource "aws_instance" "public" {
 resource "aws_security_group" "example" {
     name_prefix = "${var.name}-sg" #Security group name, e.g. jazeel-terraform-security-group
     description = "Security group for EC2"
-    vpc_id = data.aws_vpc.default.id #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
+    vpc_id = data.aws_vpc.selected.id #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
     lifecycle {
       create_before_destroy = true
     }
